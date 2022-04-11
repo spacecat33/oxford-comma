7845d11d6b39bcd74d38ed30c8e6d7279d79a4f1
Janan Akkad <jananakkad@hotmail.co.uk>
2022-04-11 13:37:41 +0100

    Completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
     if array.length == 1
-    return array.join
+    array.join
     elsif array.length == 2
-    return array.join (" and ")
+    array.join (" and ")
     elsif array.length == 3
     array[2] = "and #{array[2]}"
     array.join (', ') 
