1410b34f7416a55374359def87b39defc02997cf
Janan Akkad <jananakkad@hotmail.co.uk>
2022-04-11 15:07:52 +0100

    Completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -22,3 +22,17 @@ def oxford_comma(array)
     array.join(" and ")
     end
 end
+
+
+#or
+
+def oxford_comma(array)
+    if array.length >= 3
+    last_element = array [-1]
+    array.pop() 
+    array << "and #{last_element}" 
+    array.join (', ')
+    else 
+    array.join(" and ")
+    end
+end
